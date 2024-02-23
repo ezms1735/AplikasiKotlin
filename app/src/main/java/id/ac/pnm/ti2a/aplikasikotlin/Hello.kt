9b846922ce24d4f73c1dc1ba032092aec446a11e
Ergi Zenila Marta Sasmige <ergizms@gmail.com>
2024-02-23 11:28:29 +0700

    Biodata

--- app/src/main/java/id/ac/pnm/ti2a/aplikasikotlin/Hello.kt
+++ app/src/main/java/id/ac/pnm/ti2a/aplikasikotlin/Hello.kt
@@ -1,10 +1,14 @@
 package id.ac.pnm.ti2a.aplikasikotlin
 
-fun main(args: Array<String>) {
-    println("Hello, ${args[0]}")
-}
 
-fun printHello(name: String?): Unit {
-    println("Hi")
+fun main(args: Array<String>) {
+    biodata("Ergi Zenila Marta Sasmige", institusi = "Politeknik Negeri Madiun", hobby="Membaca", desc = "Saya suka membaca webtoon dan menonton film, saya juga suka pantai.")
+}
+fun biodata(nama:String, institusi:String, hobby:String, desc: String, kelas:String = "2A") {
+    println("Nama   : $nama")
+    println("Prodi  : $institusi")
+    println("Kelas  : $kelas")
+    println("Hobby  : $hobby")
+    println("$desc")
 }
 
