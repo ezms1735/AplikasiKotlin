6c61a9b394a49130101b29ad88c5bd78e09403a9
Ergi Zenila Marta Sasmige <ergizms@gmail.com>
2024-02-26 19:04:38 +0700

    Menambahkan Functions

---
+++ app/src/main/java/id/ac/pnm/ti2a/aplikasikotlin/CompactFunctions.kt
@@ -0,0 +1,18 @@
+package id.ac.pnm.ti2a.aplikasikotlin
+
+
+fun isTooHot(temperature: Int) = temperature > 30
+fun isDirty(dirty: Int) = dirty > 30
+fun isSunday(day: String) = day == "Sunday"
+
+fun shloudChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
+}
+fun main () {
+
+}
